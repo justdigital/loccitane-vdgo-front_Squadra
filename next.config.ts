dd71abe1d4daf3a853279248cddda01129fc61a8
Celio <celio.coti@squadra.com.br>
2025-03-13 19:43:16 -0300

    added devops code

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,14 @@ const nextConfig: NextConfig = {
   sassOptions: {
     additionalData: `$locci-red: #C02031;`,
   },
+  eslint: {
+    // Ignora erros de ESLint durante o build
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // Ignora erros de checagem de tipos durante o build
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
