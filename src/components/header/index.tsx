d91675bda40895f22cf09453f508d68f2592b2cf
Jorge Luis Malaquias <Squadra.JORGEBARBOSA.ext@loccitane.com>
2025-03-21 14:17:42 -0300

    chore: mudança dos botões do header para somente 'login'

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
 import css from './style.module.scss';
+import LoginIcon from '@mui/icons-material/Login';
 
 interface HeaderProps {}
 
@@ -11,8 +12,17 @@ const Header: React.FC<HeaderProps> = () => {
 
       <div className={`${css['icon-buttons']}`}>
         <ul className='flex space-between items-center'>
-          <li className="whatsapp"><a href={''} target="_blank"><Image src="/assets/images/icons/whatsapp.svg" alt="Whatsapp" width={37} height={37} /></a></li>
-          <li className={css.chat}><a href={''} target="_blank"><Image src="/assets/images/icons/chat.svg" alt="Chat" width={24} height={24} /></a></li>
+          <li className={`${css['login']}`}>
+            <a href={''} target="_blank" className='flex items-center gap-2'>
+              <span className={`${css['login-text']}`}>
+                <span>Lo</span>gin
+              </span>
+              <LoginIcon />
+              {/* <Image src="/assets/images/icons/whatsapp.svg" alt="Whatsapp" width={37} height={37} /> */}
+            </a>
+            </li>
+          {/* <li className="whatsapp"><a href={''} target="_blank"><Image src="/assets/images/icons/whatsapp.svg" alt="Whatsapp" width={37} height={37} /></a></li> */}
+          {/* <li className={css.chat}><a href={''} target="_blank"><Image src="/assets/images/icons/chat.svg" alt="Chat" width={24} height={24} /></a></li> */}
         </ul>
       </div>
     </div>
